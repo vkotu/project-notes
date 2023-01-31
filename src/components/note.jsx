9014210cd1169e29eeaff66ea0e7335227d98171
vkotu <vkotu@paypal.com>
2023-01-30 18:55:49 -0800

    fallback suspense

--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,9 +1,20 @@
-import Editing from './edit';
+// import Editing from '';
+import { lazy, Suspense } from 'react';
+import { simulateNetwork } from '../lib/sleep';
+import Loading from './loading';
+
+const Editing = lazy(() => simulateNetwork(import('./edit')));
 
 const NoteContent = ({ note, setNotes, isEditing }) => {
-  if (isEditing) return <Editing note={note} setNotes={setNotes} />;
+  if (isEditing) {
+    return (
+      <Suspense fallback={<Loading />}>
+        <Editing note={note} setNotes={setNotes} />;
+      </Suspense>
+    );
+  }
   return (
-    <div className="flex flex-col h-full gap-4 p-4 overflow-y-scroll">
+    <div className="flex h-full flex-col gap-4 overflow-y-scroll p-4">
       <h2>{note.title}</h2>
       <p>{note.content}</p>
     </div>
